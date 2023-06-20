b6886104016e897d74cec53312cb24ef997063f6
IshaniAryan <132250638+IshaniAryan@users.noreply.github.com>
2023-06-20 18:05:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,13 +18,13 @@ terraform {
 provider "snowflake" {
 }
 
-resource "snowflake_database" "util_db" {
-  name    = "UTIL_DB"
+resource "snowflake_database" "demo_db" {
+  name    = "DEMO_DB"
   comment = "Database for Snowflake Terraform demo"
 }
 
-resource "snowflake_schema" "util_schema" {
-  database = snowflake_database.util_db.name
-  name     = "UTIL_SCHEMA"
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db.name
+  name     = "DEMO_SCHEMA"
   comment  = "Schema for Snowflake Terraform demo"
 }
